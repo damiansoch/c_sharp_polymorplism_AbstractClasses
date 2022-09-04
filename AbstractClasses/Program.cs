3846f7934345599dc39d2b5601e4ca0b3d797df5
damiansoch <66294165+damiansoch@users.noreply.github.com>
2022-09-04 09:05:31 +0100

    Add project files.

---
+++ AbstractClasses/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace AbstractClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cube cube = new(12.54);
+            cube.GetInfo();
+
+            Sphere sphere = new(5.1);
+            sphere.GetInfo();
+
+            Console.WriteLine("\n");
+            //or
+            Shape[] shapes = { new Cube(1.54), new Sphere(47.1) };
+
+            foreach (Shape shape in shapes)
+            {
+                Console.WriteLine($"{shape.Name} volume: {shape.Volume()}");
+
+                //as keyword
+                //creating variable as a certain type
+                Cube iceCube = shape as Cube;
+                if(iceCube == null)
+                {
+                    Console.WriteLine("This shape is not a Cube");
+                }
+
+                if(shape is Cube)
+                {
+                    Console.WriteLine("This shape is a cube");
+                }
+
+
+            }
+            Console.ReadLine();
+
+            
+
+            
+        }
+    }
+}
---
+++ AbstractClasses/Sphere.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+    internal class Sphere : Shape
+    {
+        public double Radius { get; set; }
+
+        public Sphere(double radius)
+        {
+            Name = "Sphere";
+            Radius = radius;
+        }   
+
+        public override double Volume()
+        {
+            return (4 * Math.PI * (Math.Pow(Radius,3)))/3;
+        }
+
+        public override void GetInfo()
+        {
+            Console.WriteLine($"\n This is a {Name}, and has a volume of {Volume()}");
+        }
+    }
+}
